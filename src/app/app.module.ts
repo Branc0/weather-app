9042c6834d7371474ffeb5c13bad13a51e769754
Rafael Rocha de azevedo <rafaelrochadeazevedo@Mac-mini-de-Rafael.local>
2022-01-17 18:54:02 -0300

    feat: adds service worker notification for new versions available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,18 @@ import { WeatherComponent } from './weather/weather.component';
 import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +28,9 @@ import { environment } from '../environments/environment';
     MatIconModule,
     MatCardModule,
     MatInputModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
