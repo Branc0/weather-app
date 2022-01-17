7821836fa13308b65a0275a49de80087b0d72dfd
Rafael Rocha de azevedo <rafaelrochadeazevedo@Mac-mini-de-Rafael.local>
2022-01-17 13:24:29 -0300

    sets enviroments variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "api.openweathermap.org/data/2.5/",
+  apiKey: "08c0297f6a36d6c35ec89ac5c33417c0"
 };
 
 /*
